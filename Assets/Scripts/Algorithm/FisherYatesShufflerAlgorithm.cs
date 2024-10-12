98506131ca2cb1b3c24f242c797ba24ed5fc2c99
Linh_da_ra <khanhtran2002nd@gmail.com>
2024-10-13 03:20:26 +0700

    FisherYatesShufflerAlgorithm

---
+++ Assets/Scripts/Algorithm/FisherYatesShufflerAlgorithm.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class FisherYatesShufflerAlgorithm
+{
+    public static void Shuffle<T>(List<T> _list)
+    {
+        Random rng = new Random();
+        for (int i = _list.Count - 1; i >= 0; i--)
+        {
+            int j = rng.Next(i + 1);
+            T temp = _list[j];
+            _list[i] = _list[j];
+            _list[j] = temp;
+        }
+    }
+}
